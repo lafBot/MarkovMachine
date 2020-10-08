cf949b2b5ba94fb879049918c531a7b9ea5e3fea
lafBot <64456835+lafBot@users.noreply.github.com>
2020-10-08 11:09:00 -0600

    Completed makeText file to include functions for creating markov script through node command

--- makeText.js
+++ makeText.js
@@ -6,16 +6,41 @@ const markov = require("./markov");
 const axios = require("axios");
 const process = require("process");
 
-let path = 'eggs.txt'
-
-function makeText() {
+function makeTextFromFile(path) {
     fs.readFile(path, 'utf8', function(err, data) {
         if (err) {
             console.error("There was an error reading the file: ", err);
             process.kill(1);
+        } else {
+            let result = new markov.MarkovMachine(data);
+            console.log(result.makeText());
         }
-
-        let product = new markov.MarkovMachine(data)
-        console.log(product.makeText())
     })
+}
+
+async function makeTextFromURL(path) {
+    const source = await axios.get(path);
+    let result;
+    try {
+        result = new markov.MarkovMachine(source.data);
+        console.log(result.makeText());
+    } catch (err) {
+        console.error("Unable to retrieve source data:", err);
+        process.kill(1);
+    }
+}
+
+let [method, path] = process.argv.slice(2);
+
+if (method == 'file') {
+    makeTextFromFile(path);
+}
+
+else if (method == 'url') {
+    makeTextFromURL(path);
+}
+
+else {
+    console.error(`Method ${method} unknown`);
+    process.kill(1);
 }
